derivatives wrong

pyfoil/pyFoil.py
@@ -782,6 +782,28 @@ class Airfoil(object):
 
         return opt_point[0], opt_point[1]
 
+    def _findChordProj(self, coord):
+        """
+        Finds the point on the chordline that defines a line from `coord` to the chordline that is perpendicular to the chordline
+
+        Parameters
+        ----------
+        coord : 2darray
+            The point of interest we wish to project onto the chordline.
+
+        Returns
+        -------
+        point : 2darray
+            The coordinate that is the perpendicular projection of `coord` onto the chordline
+        """
+        # vector defines the chord
+        chord = self.LE - self.TE
+
+        # Parametric position of point on chordline
+        s = (-chord[0] * (self.TE[0] - coord[0]) - chord[1] * (self.TE[1] - coord[1])) / (chord[0] ** 2 + chord[1] ** 2)
+
+        return self.TE + s * chord
+
     def _MaxCamberOptimize(self, maximum):
         """
         Used to compute the most negative and most positive cambers of an airfoil
@@ -802,10 +824,26 @@ class Airfoil(object):
         """
 
         def f(s, factor):
-            return factor * self.camber.getValue(s)[1]
+            chordProj = self._findChordProj(self.camber.getValue(s))
+            return factor * np.linalg.norm(self.camber.getValue(s) - chordProj)
 
         def df(s, factor):
-            return factor * self.camber.getDerivative(s)[1]
+            # Derivative of camber point with respect to spline parameter (point of interest)
+            dPI = self.camber.getDerivative(s)
+
+            # Derivative of chord parameter from _getChordProj
+            chord = self.LE - self.TE
+            da = (chord[0] * dPI[0] + chord[1] * dPI[1]) / np.linalg.norm(chord)**2
+
+            # Derivative of intersection point
+            dIN = np.array([chord[0] * da, chord[1] * da])
+
+            # derivative of the distance we are maximizing or minimizing
+            PI = self.camber.getValue(s)
+            IN = self._findChordProj(PI)
+            dd = ((PI[0] - IN[0]) * (dPI[0] - dIN[0]) + (PI[1] - IN[1]) * (dPI[1] - dIN[1])) / np.linalg.norm(PI - IN)
+
+            return factor * dd
 
         if maximum:
             factor = -1
@@ -822,7 +860,14 @@ class Airfoil(object):
 
         opt_point = self.camber.getValue(opt.x)
 
-        return opt_point[0], opt_point[1]
+        opt_int = self._findChordProj(opt_point)
+
+        print(opt_point)
+        # convert to airfoil coordinates
+        x = np.linalg.norm(opt_int - self.LE) / np.linalg.norm(self.LE - self.TE)
+        c = -factor * np.linalg.norm(opt_point - opt_int) / np.linalg.norm(self.LE - self.TE)
+
+        return x, c
 
     def getMaxCamber(self):
         """

tests/test_pyfoil.py
@@ -104,6 +104,20 @@ class TestBasic(unittest.TestCase):
         self.assertAlmostEqual(self.chord, self.foil.getChord())
         self.assertAlmostEqual(self.twist, self.foil.getTwist())
 
+    def test_chordProj_basic(self):
+        val = self.foil._findChordProj(np.array([0.57, 2]))
+        assert_allclose(np.array([0.57, 0]), val, atol=1e-12)
+
+    def test_chordProj_vertical(self):
+        self.foil.rotate(-90)
+        val = self.foil._findChordProj(np.array([3.7, -0.76]))
+        assert_allclose(np.array([0, -0.76]), val, atol=1e-12)
+
+    def test_chordProj_angle(self):
+        self.foil.rotate(-30)
+        val = self.foil._findChordProj(np.array([0.5, 0]))
+        assert_allclose(np.array([3 / 8, -np.sqrt(3) / 8]), val, atol=1e-12)
+
 
 class TestSampling(unittest.TestCase):
     def setUp(self):
@@ -295,11 +309,26 @@ class TestCamber(unittest.TestCase):
 
     def test_rae2822_thickness(self):
         maxThickness = self.foil.getMaxThickness("british")
-        np.testing.assert_allclose(maxThickness, [0.379, 0.121], rtol=1e-2)
+        assert_allclose(maxThickness, [0.379, 0.121], rtol=1e-2)
 
     def test_rae2822_camber(self):
         maxCamber = self.foil.getMaxCamber()
-        np.testing.assert_allclose(maxCamber, [0.757, 0.013], rtol=0.15)
+        assert_allclose(maxCamber, [0.757, 0.013], rtol=0.19)
+
+    def test_rae2822_camber_scale(self):
+        self.foil.scale(1.5)
+        maxCamber = self.foil.getMaxCamber()
+        assert_allclose(maxCamber, [0.757, 0.013], rtol=0.19)
+
+    def test_rae2822_camber_translate(self):
+        self.foil.translate([0.2, -0.23])
+        maxCamber = self.foil.getMaxCamber()
+        assert_allclose(maxCamber, [0.757, 0.013], rtol=0.19)
+
+    def test_rae2822_camber_rot(self):
+        self.foil.rotate(-30)
+        maxCamber = self.foil.getMaxCamber()
+        assert_allclose(maxCamber, [0.757, 0.013], rtol=0.19)
 
 
 class TestFileWriting(unittest.TestCase):